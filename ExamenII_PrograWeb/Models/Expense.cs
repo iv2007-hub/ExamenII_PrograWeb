7a89c3b861017f1e7b07581bbc0d7dcc253ba40e
iv2007-hub <ivanna_trujillo@unitec.edu>
2026-06-27 14:06:41 -0600

    modelos

---
+++ ExamenII_PrograWeb/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenII_PrograWeb.Models;
+
+public class Expense
+{
+    public string Id { get; set; }          
+    public string Description { get; set; } 
+    public decimal Amount { get; set; }      
+    public string Category { get; set; }     
+    public string UserId { get; set; }       
+    public DateTime CreatedAt { get; set; } 
+}
---
+++ ExamenII_PrograWeb/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenII_PrograWeb.Models;
+
+
+    public class User
+    {
+        public string Id { get; set; }           
+        public string FullName { get; set; }      
+        public string Email { get; set; }         
+        public string PasswordHash { get; set; }  
+    }
